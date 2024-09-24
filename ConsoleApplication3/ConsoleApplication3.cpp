770a9e9809d785b9766b5ee0ff355275df657d65
se1fa <se1fa@SE1FA1>
2024-09-24 13:16:58 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int a;
+    int b = 0;
+    std::cout << "Введите  число: \n";
+    std::cin >> a;
+    while (a != 0)
+    {
+       b += a;
+       std::cin >> a;
+    }
+    std::cout << "Сумма: " << b;
+}
+
+
